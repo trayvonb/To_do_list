158cd860e06e72dc31c5a1543d1b83c4b26ab067
trayvonb <93352324+trayvonb@users.noreply.github.com>
2021-12-02 23:30:19 +0000

    create my_To_Do_List

--- todo.py
+++ todo.py
@@ -1,1 +1,17 @@
-
+my_To_Do_List = ["buy christmas presents", " buy clothes", "wash shirts"]
+#print(my_To_Do_List)
+#print(len(my_To_Do_List))
+#print(my_To_Do_List.index("buy christmas presents"))
+print("my_To_Do_List")
+for task in my_To_Do_List:
+ print (task)
+ print("what would you like to add?")
+ add = input()
+my_To_Do_List.append(add)
+for task in my_To_Do_List:
+  print (task)
+  print("what would you like to remove?")
+  remove = input()
+  my_To_Do_List.remove(remove)
+  for task in my_To_Do_List:
+    print(task) 
