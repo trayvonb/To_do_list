7456eee6d2796abe30d8ea039b2e60f0c37f0521
Tray <93352324+trayvonb@users.noreply.github.com>
2021-12-16 23:17:19 +0000

    my_To_Do_List

--- todo.py
+++ todo.py
@@ -1,25 +1,30 @@
-my_To_Do_List = []
-#print(my_To_Do_List)
-#print(len(my_To_Do_List))
-#print(my_To_Do_List.index("buy christmas presents"))
-# print("my_To_Do_List")
+my_To_Do_List=("buy new clothes for school", " buy more video games", " buy football eqiptment")
+
+# # print("my_To_Do_List")
+# # for task in my_To_Do_List:
+# #  print (task)
+# #  print("what would you like to add?")
+# #  add = input()
+# # my_To_Do_List.append(add)
+# # for task in my_To_Do_List:
+# #   print (task)
+# #   print("what would you like to remove?")
+# #   remove = input()
+# #   my_To_Do_List.remove(remove)
+# #   for task in my_To_Do_List:
+# #     print(task) 
+# if len(my_To_Do_List) == 0:
+#   print ("what would you like to add?")
+# add = input()
+# my_To_Do_List.append(add)
+
+# print("My To Do List")
 # for task in my_To_Do_List:
 #  print (task)
-#  print("what would you like to add?")
-#  add = input()
-# my_To_Do_List.append(add)
-# for task in my_To_Do_List:
-#   print (task)
-#   print("what would you like to remove?")
-#   remove = input()
-#   my_To_Do_List.remove(remove)
-#   for task in my_To_Do_List:
-#     print(task) 
-if len(my_To_Do_List) == 0:
-  print ("what would you like to add?")
+def my_function():
+  print("my_To_Do_List")
+
+my_function("my_To_Do_List")
 add = input()
 my_To_Do_List.append(add)
-
-print("My To Do List")
-for task in my_To_Do_List:
- print (task)
+print(my_To_Do_List)
